6b47c7bf8d967160c5cb4d7d01c9e6028b1dcac3
tangwenkai <twk@shfangyan.cn>
2018-07-16 10:14:47 +0800

    Update CI

--- CI.sh
+++ CI.sh
@@ -1,9 +1,7 @@
 #/bin/bash
 
-git submodule update
-
-qmake && make -j`nproc` && make clean
-qmake && make -j`nproc` && make distclean
+#qmake && make -j`nproc` && make clean
+#qmake && make -j`nproc` && make distclean
 
 source /opt/davy/1.0/environment-setup-cortexa9hf-vfp-neon-davy-linux-gnueabi
 qmake && make -j`nproc` && make clean
